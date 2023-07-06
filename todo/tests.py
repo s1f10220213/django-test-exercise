99dd05dd5636ab11f69d0080cc92ac49e6168cbb
1f10220213 <s1f102202134@iniad.org>
2023-07-06 11:37:10 +0900

    警告回避2

--- todo/tests.py
+++ todo/tests.py
@@ -75,7 +75,7 @@ class TodoViewTestCase(TestCase):
     def test_index_get_order_post(self):
         task1 = Task(title='task1', due_at = timezone.make_aware(datetime(2023, 7, 1)))
         task1.save()
-        task2= Task(title= 'task2', due_at = timezone.make_aware(datetime(2023, 8, 1)))
+        task2 = Task(title= 'task2', due_at = timezone.make_aware(datetime(2023, 8, 1)))
         task2.save()
         client = Client()
         response = client.get('/?order=post')
@@ -86,9 +86,9 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context['tasks'][1], task1)
 
     def test_index_get_order_due(self):
-        task1 = Task(title= 'task1', due_at=timezone.make_aware(datetime(2023, 7, 1)))
+        task1 = Task(title= 'task1', due_at = timezone.make_aware(datetime(2023, 7, 1)))
         task1.save()
-        task2 = Task(title= 'task2', due_at=timezone.make_aware(datetime(2023, 8, 1)))
+        task2 = Task(title= 'task2', due_at = timezone.make_aware(datetime(2023, 8, 1)))
         task2.save()
         client = Client()
         response = client.get('/?order=due')
